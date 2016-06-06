efe42d9ee400f299d8389bb8beafe8923605ef5c
Garrett LeSage <garrett@lesage.us>
2016-06-06 18:13:54 +0200

    handle URLs that begin with //

--- lib/wiki_helpers.rb
+++ lib/wiki_helpers.rb
@@ -13,6 +13,8 @@ class WikiHelpers < Middleman::Extension
       match_redir = redirects.map do |line|
         splits = line.split(':')
 
+        #return nil if splits.first == '---' || splits.first.empty?
+
         { from: splits.first.strip, to: splits.last.strip }
       end.compact
 
@@ -20,31 +22,41 @@ class WikiHelpers < Middleman::Extension
     end
 
     def find_wiki_page(searchkey)
+      puts "KEY #{searchkey}"
       extra = /[#\?].*/
       url_extra = searchkey.match(extra).to_s
-      url_fixed = searchkey.tr('_', ' ').gsub(extra, '')
+      url_fixed = searchkey.tr('_', ' ').gsub(url_extra, '')
 
       searchkey.gsub!(extra, '')
 
       match_redir = load_redirects.select do |redir|
-        #redir[:to].downcase if redir[:from].match(/(^|\/)#{searchkey}$/i)
-        redir[:to].downcase if redir[:from].end_with? "/#{searchkey.downcase}"
+        #redir[:to].downcase if redir[:from].match(/(^|\/)#{url_fixed}$/i)
+        redir[:to] if redir[:from].downcase.end_with? "/#{url_fixed.downcase}"
       end
 
-      sitemap.resources.select do |resource|
+      result = sitemap.resources.select do |resource|
         next unless resource.data.wiki_title
 
         # Check direct matches
         matches ||= resource.data.wiki_title.to_s.downcase.strip == url_fixed.tr('_', ' ').downcase.strip
-        #matches ||= resource.data.wiki_title.strip[/(^|\/)#{searchkey}$/i]
-        #matches ||= resource.data.wiki_title.downcase.strip.end_with? "/#{searchkey.downcase}"
+        #matches ||= resource.data.wiki_title.strip[/(^|\/)#{url_fixed}$/i]
+        matches ||= resource.data.wiki_title.downcase.strip.end_with? "/#{url_fixed.downcase}"
         # Handle redirects
-        matches ||= match_redir.include? resource.data.wiki_title.downcase
+        #matches ||= match_redir.include? resource.data.wiki_title.downcase
+        #matches ||= match_redir.select { |m| m[:from].downcase.end_with? url_fixed.downcase }.count > 0
+
+        # puts "REDIRS" + match_redir.select { |m| m[:from].end_with? url_fixed }.to_s if matches
+
+        # puts "MATCHES #{matches}" if matches
 
         matches
       end.map do |resource|
         resource.url + url_extra
       end.first
+
+      puts "END #{result}"
+
+      result
     end
   end
 end
