80af8c84852ab065f050192be3ca4d4becab3825
Garrett LeSage <garrett@lesage.us>
2016-06-07 14:58:15 +0200

    almost finally there? (I HOPE!)

--- lib/wiki_helpers.rb
+++ lib/wiki_helpers.rb
@@ -8,53 +8,82 @@ class WikiHelpers < Middleman::Extension
     def load_redirects
       return $helper_wiki_redirects if $helper_wiki_redirects
 
+      slashes = /^\/|\/$/
+
       redirects = File.readlines "#{root}/#{source}/redirects.yaml"
 
-      match_redir = redirects.map do |line|
+      $helper_wiki_redirects ||= redirects.map do |line|
         splits = line.split(':')
 
-        #return nil if splits.first == '---' || splits.first.empty?
-
-        { from: splits.first.strip, to: splits.last.strip }
+        {
+          from: splits.first.gsub(slashes, '').strip,
+          to: splits.last.gsub(slashes, '').strip
+        }
       end.compact
-
-      $helper_wiki_redirects ||= match_redir
     end
 
     def find_wiki_page(searchkey)
-      puts "KEY #{searchkey}"
+      # puts "KEY #{searchkey}"
+      #searchkey.strip!
+      searchkey.sub!(/^ /, '#') # Weird wiki-ism
+
       extra = /[#\?].*/
-      url_extra = searchkey.match(extra).to_s
-      url_fixed = searchkey.tr('_', ' ').gsub(url_extra, '')
+      url_extra = searchkey
+                  .match(extra).to_s
+                  .tr('_', '-').tr(' ', '-')
+                  .downcase.squeeze('-')
 
-      searchkey.gsub!(extra, '')
+      url_fixed = searchkey
+                  .sub(extra, '').tr('_', ' ')
+                  .gsub(/^\/|\/$/, '')
+                  .downcase
 
-      match_redir = load_redirects.select do |redir|
-        #redir[:to].downcase if redir[:from].match(/(^|\/)#{url_fixed}$/i)
-        redir[:to] if redir[:from].downcase.end_with? "/#{url_fixed.downcase}"
-      end
+      #searchkey.gsub!(extra, '')
+      #
+      # puts url_fixed
+
+      match_redir = load_redirects.map do |redir|
+        next if url_fixed.empty?
+        if redir[:from].downcase.end_with? url_fixed.downcase
+          redir[:to].downcase.tr('_', ' ')
+        end
+      end.compact
 
       result = sitemap.resources.select do |resource|
         next unless resource.data.wiki_title
 
+        wiki_title = resource.data.wiki_title.to_s.downcase.strip
+
         # Check direct matches
-        matches ||= resource.data.wiki_title.to_s.downcase.strip == url_fixed.tr('_', ' ').downcase.strip
-        #matches ||= resource.data.wiki_title.strip[/(^|\/)#{url_fixed}$/i]
-        matches ||= resource.data.wiki_title.downcase.strip.end_with? "/#{url_fixed.downcase}"
+        matches ||= wiki_title == url_fixed
+
+        # Look for features
+        #matches ||= resource.data.feature_name.to_s.tr('_', ' ').downcase == url_fixed.tr('_', ' ').downcase
+        # matches ||= wiki_title == "#{url_fixed.downcase}"
+
         # Handle redirects
-        #matches ||= match_redir.include? resource.data.wiki_title.downcase
-        #matches ||= match_redir.select { |m| m[:from].downcase.end_with? url_fixed.downcase }.count > 0
+        #matches ||= match_redir.select do |redir|
+          #if searchkey == '/'
+            #puts "#{wiki_title} == #{redir[:to].to_s.downcase}" if wiki_title == redir[:to].to_s.downcase
+          #end
+          #wiki_title == redir.to_s.downcase
+        #end.count > 0
 
-        # puts "REDIRS" + match_redir.select { |m| m[:from].end_with? url_fixed }.to_s if matches
+        #matches ||= match_redir.include? url_fixed
+        #matches ||= match_redir.include? wiki_title.scan(/[^\/]*$/)
+        #matches ||= match_redir.include? "features/#{wiki_title}".downcase
 
-        # puts "MATCHES #{matches}" if matches
+        # Handle redirects
+        matches ||= match_redir.include? wiki_title
+
+        # puts "inc: " + match_redir.join(" :: ") if matches
 
         matches
       end.map do |resource|
         resource.url + url_extra
       end.first
 
-      puts "END #{result}"
+      # puts "END #{result}"
 
       result
     end
